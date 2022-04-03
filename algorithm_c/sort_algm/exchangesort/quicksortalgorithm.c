13972f2316d0afcb8754be39f82b71d8490100dd
kyrielucy <kyrielucy@gmail.com>
2022-04-03 16:27:58 +0800

    change qsort.c file

--- algorithm_c/sort_algm/exchangesort/quicksortalgorithm.c
+++ algorithm_c/sort_algm/exchangesort/quicksortalgorithm.c
@@ -29,7 +29,7 @@ void quick_sort(int arr[], int ibegin, int iend) //数组元素插入法，
 /** ①先从队尾开始向前扫描且当low < high时,如果a[high] > tmp,则high–,但如果a[high] < tmp,
  * 则将high的值赋值给low,即arr[low] = a[high],同时要转换数组扫描的方式,即需要从队首开始向队尾进行扫描了
  *  ②同理,当从队首开始向队尾进行扫描时,如果a[low] < tmp,则low++,但如果a[low] > tmp了,
- * 则就需要将low位置的值赋值给high位置,即arr[low] = arr[high],同时将数组扫描方式换为由队尾向队首进行扫描.
+ * 则就需要将low位置的值赋值给high位置,即arr[high] = arr[low],同时将数组扫描方式换为由队尾向队首进行扫描.
  *  ③不断重复①和②,知道low>=high时(其实是low=high),low或high的位置就是该基准数据在数组中的正确索引位置.
  */
 void quick_sort(int arr[], int ibegin, int iend) //数组元素交换法
